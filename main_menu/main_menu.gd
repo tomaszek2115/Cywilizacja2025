ed8ab7094d7e22f01156eee2ce4ad0e34e754c8d
tomaszek2115 <tomaush02@gmail.com>
2024-11-17 21:23:14 +0100

    added simple load game scene

--- main_menu/main_menu.gd
+++ main_menu/main_menu.gd
@@ -13,10 +13,10 @@ func _process(delta: float) -> void:
 
 func _on_newGame_pressed() -> void:
 	print("New Game pressed")
-	#get_tree().change_scene_to_file()
 
 func _on_loadGame_pressed() -> void:
 	print("Load Game pressed")
+	get_tree().change_scene_to_file("res://load_game/load_game.tscn")
 
 func _on_settings_pressed() -> void:
 	get_tree().quit() #potem sie doda reszte
